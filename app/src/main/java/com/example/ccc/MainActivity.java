a2db626834a1cfe382f776973f515d200dbaf136
sshworld <ssh97540@gmail.com>
2020-08-24 22:32:19 +0900

    맵 설정 및 로그인 설정

--- app/src/main/java/com/example/ccc/MainActivity.java
+++ app/src/main/java/com/example/ccc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ccc;
 
+import android.os.Bundle;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import net.daum.android.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+
+        mapViewContainer.addView(mapView);
+
     }
+
+
 }
